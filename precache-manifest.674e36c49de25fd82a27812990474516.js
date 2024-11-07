caf64d8a202842c68aba72d0a0adc34695174d46
root <root@ip-172-31-1-47.ap-south-1.compute.internal>
2024-11-07 19:27:13 +0000

    Updates

---
+++ precache-manifest.674e36c49de25fd82a27812990474516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0151192575fdfe61e419eb640cd465fb",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "3ba2cf3ef69a0cc54b85",
+    "url": "/netflix-clone/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4d546a2f74451e707dbb",
+    "url": "/netflix-clone/static/js/2.3b16712d.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/netflix-clone/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "3ba2cf3ef69a0cc54b85",
+    "url": "/netflix-clone/static/js/main.9d9d0e2c.chunk.js"
+  },
+  {
+    "revision": "92b6ea16ae94295cbdcc",
+    "url": "/netflix-clone/static/js/runtime~main.2007c77f.js"
+  }
+]);
